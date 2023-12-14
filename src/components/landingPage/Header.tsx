9186d0bbfccdd80e722d34c68566c408ace5d50a
pockypoem <jeremyas.cornelis@gmail.com>
2023-12-14 13:26:39 +0700

    fix: fixing navbar and herosection layout

--- src/components/landingPage/Header.tsx
+++ src/components/landingPage/Header.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const imageStyle = {
@@ -5,81 +6,112 @@ const imageStyle = {
 }
 
 const Header = () => {
-  return (
-    <header className="header">
 
-        {/* Overlay */}
-        <div className="overlay" data-overlay></div>
+    const [isNavActive, setIsNavActive] = useState(false);
 
-        <div className="container">
+    useEffect(() => {
+        const elemToggleFunc = (elem: Element | null) => elem?.classList.toggle("active");
 
-            <a href="/" className="logo">
-                <img src="https://i.ibb.co/MkygZR1/bcr-logo.png" alt="bcr-logo" style={imageStyle}/>
-            </a>
+        const navbar = document.querySelector("[data-nav]");
+        const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+        const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+        const overlay = document.querySelector("[data-overlay]");
 
-            <a className="nav-open-btn" data-nav-open-btn>
-                <img src="https://i.ibb.co/0VBC9mX/fi-menu.png" alt="fi-menu" style={imageStyle} width="24" height="24" />
-            </a>
+        const navElemArr = [navOpenBtn, navCloseBtn, overlay];
 
-            <nav className="navigasi" data-nav>
+        const handleNavClick = () => {
+            // console.log("Nav Clickedd")
+            document.body.classList.toggle("active");
+            elemToggleFunc(navbar);
+            elemToggleFunc(overlay);
+            elemToggleFunc(document.body);
+            setIsNavActive(!isNavActive);
+        };
 
-                <div className="navigasi-top">
-                    <h4 className="text-logo"><a href="/" style={{ textDecoration: 'none', color: 'black' }}>BCR</a></h4>
+        navElemArr.forEach(elem => elem?.addEventListener("click", handleNavClick));
+
+        // clean the event listener after components remove
+        return() => {
+            navElemArr.forEach((elem) => elem?.removeEventListener("click", handleNavClick));
+        }
+
+    }, [isNavActive])
+
+    return (
+        <header className="header">
+
+            {/* Overlay */}
+            <div className={`overlay ${isNavActive ? "active" : ""}`} data-overlay></div>
+
+            <div className="container">
+
+                <a href="/" className="logo">
+                    <img src="https://i.ibb.co/MkygZR1/bcr-logo.png" alt="bcr-logo" style={imageStyle}/>
+                </a>
+
+                <a className="nav-open-btn" data-nav-open-btn>
+                    <img src="https://i.ibb.co/0VBC9mX/fi-menu.png" alt="fi-menu" style={imageStyle} width="24" height="24" />
+                </a>
+
+                <nav className="navigasi" data-nav>
+
+                    <div className="navigasi-top">
+                        <h4 className="text-logo"><a href="/" style={{ textDecoration: 'none', color: 'black' }}>BCR</a></h4>
 
 
-                    <a className="nav-close-btn" data-nav-close-btn>
-                        <img src="https://i.ibb.co/Wk7Vbhb/fi-x.png" alt="fi-x" style={imageStyle} width="24" height="24" />
-                    </a>
-                </div>
-
-                <ul className="navigasi-list">
-
-                    <li>
-                        <a href="#service" className="navigasi-link text-dark ">Our Services</a>
-                    </li>
-
-                    <li>
-                        <a href="#about" className="navigasi-link text-dark">Why Us</a>
-                    </li>
-
-                    <li>
-                        <a href="#testimoni" className="navigasi-link text-dark">Testimony</a>
-                    </li>
-
-                    <li>
-                        <a href="#faq" className="navigasi-link text-dark" style={{ width: '120px', marginRight: '20px' }}>FAQ</a>
-                    </li>
-
-                </ul>
-
-                <ul className="navigasi-register">
-                    <li>
-                        <button className="btn-green">
-                            <div className="btn">
-                                <Link to="/dashboard" className="white">Register</Link>
-                                {/* Register */}
-                            </div>
-                        </button>
-                    </li>
-                </ul>
-
-            </nav>
-
-            <div className="header-actions">
-
-                <button className="btn-green">
-                    <div className="btn">
-                        <Link to="/dashboard" style={{ textDecoration: 'none', color: 'white' }} className="white">Register</Link>
-                        {/* Register */}
+                        <a className="nav-close-btn" data-nav-close-btn>
+                            <img src="https://i.ibb.co/Wk7Vbhb/fi-x.png" alt="fi-x" style={imageStyle} width="24" height="24" />
+                        </a>
                     </div>
-                </button>
+
+                    <ul className="navigasi-list">
+
+                        <li>
+                            <a href="#service" className="navigasi-link text-dark ">Our Services</a>
+                        </li>
+
+                        <li>
+                            <a href="#about" className="navigasi-link text-dark">Why Us</a>
+                        </li>
+
+                        <li>
+                            <a href="#testimoni" className="navigasi-link text-dark">Testimony</a>
+                        </li>
+
+                        <li>
+                            <a href="#faq" className="navigasi-link text-dark" style={{ width: '120px', marginRight: '20px' }}>FAQ</a>
+                        </li>
+
+                    </ul>
+
+                    <ul className="navigasi-register" style={{  width:'200px', paddingTop: '5px', paddingBottom:'5px' }}>
+                        <li>
+                            <button className="btn-green" style={{ borderRadius: '10px' }}>
+                                <div className="btn">
+                                    <Link to="/login" className="white" style={{ textDecoration: 'none', color:'white' }}>Register</Link>
+                                    {/* Register */}
+                                </div>
+                            </button>
+                        </li>
+                    </ul>
+
+                </nav>
+
+                <div className="header-actions">
+
+                    <button className="btn-green" style={{ borderRadius: '10px', width:'100px', paddingTop: '5px', paddingBottom: '5px' }}>
+                        <div className="btn">
+                            <Link to="/login" style={{ textDecoration: 'none', color: 'white' }} className="white">Register</Link>
+                            {/* Register */}
+                        </div>
+                    </button>
+
+                </div>
 
             </div>
 
-        </div>
-
-    </header>
-  );
+        </header>
+    );
 };
 
 export default Header
--- src/components/landingPage/HeroSection.tsx
+++ src/components/landingPage/HeroSection.tsx
@@ -1,44 +1,62 @@
 import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 const HeroSection = () => {
-  return (
-    <>
-        <section className="hero" id="hero">
-            <div className="container hero-section" id="hero-section">
-                <div className="row">
-                    {/* Left Section Text */}
-                    <div className="col left-hero-section" id="left-hero-section-style">
-                        <h1 className="heading-hero">
-                            Sewa & Rental Mobil Terbaik di kawasan Semarang
-                        </h1>
-                        <p className="detail-heading detail-heading-hero-style lh-lg">
-                            Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangaku. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.
-                        </p>
-                        <button className="btn-green btn-hero">
-                            <div className="btn">
-                                <Link to="/cari-mobil" style={{ textDecoration: 'none', color: 'white' }} className="white">Mulai Sewa Mobil</Link>
-                                {/* <a href="/cars" style={{ textDecoration:'none', color: 'white' }}>Mulai Sewa Mobil</a> */}
-                            </div>
-                        </button>
-                    </div>
+
+    const [isContainerFluid, setIsContainerFluid] = useState(window.innerWidth >= 1150)
+
+    useEffect(() => {
+        const updateContainerClass = () => {
+            setIsContainerFluid(window.innerWidth >= 1150)
+        }
+
+        updateContainerClass();
+
+        window.addEventListener("resize", updateContainerClass);
+
+        return() => {
+            window.removeEventListener("resize", updateContainerClass);
+        }
+    }, []);
+
+    return (
+        <>
+            <section className="hero" id="hero">
+                <div className={`container ${isContainerFluid ? "container-fluid" : ""} hero-section`} id="hero-section">
+                    <div className="row">
+                        {/* Left Section Text */}
+                        <div className="col left-hero-section" id="left-hero-section-style">
+                            <h1 className="heading-hero">
+                                Sewa & Rental Mobil Terbaik di kawasan Semarang
+                            </h1>
+                            <p className="detail-heading detail-heading-hero-style lh-lg">
+                                Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangaku. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.
+                            </p>
+                            <button className="btn-green btn-hero" style={{ borderRadius: '10px', width:'200px' }}>
+                                <div className="btn">
+                                    <Link to="/cari-mobil" style={{ textDecoration: 'none', color: 'white' }} className="white">Mulai Sewa Mobil</Link>
+                                    {/* <a href="/cars" style={{ textDecoration:'none', color: 'white' }}>Mulai Sewa Mobil</a> */}
+                                </div>
+                            </button>
+                        </div>
 
 
-                    {/* Right Section Image */}
-                    <div className="col imagehero-section">
-                        <div className="position-relative image-wrap-hero">
-                            <div className="bottom-0 end-0">
-                                <img src="https://i.ibb.co/x320NJr/img-car.png" alt="img-car" className="imagehero-style" style={{ width: '280px', border:'0' }} />
-                                <img src="https://i.ibb.co/x320NJr/img-car.png" alt="img-car" className="image-desktop-style" style={{ width: '580px', border:'0' }}/>
-                                <img src="https://i.ibb.co/x320NJr/img-car.png" alt="img-car" className="image-desktop-style-1440" style={{ width: '700px', border:'0' }} />
+                        {/* Right Section Image */}
+                        <div className="col imagehero-section">
+                            <div className="position-relative image-wrap-hero">
+                                <div className="bottom-0 end-0">
+                                    <img src="https://i.ibb.co/x320NJr/img-car.png" alt="img-car" className="imagehero-style" style={{ width: '280px', border:'0' }} />
+                                    <img src="https://i.ibb.co/x320NJr/img-car.png" alt="img-car" className="image-desktop-style" style={{ width: '580px', border:'0' }}/>
+                                    <img src="https://i.ibb.co/x320NJr/img-car.png" alt="img-car" className="image-desktop-style-1440" style={{ width: '700px', border:'0' }} />
+                                </div>
                             </div>
                         </div>
-                    </div>
 
+                    </div>
                 </div>
-            </div>
-        </section>
-    </>
-  )
+            </section>
+        </>
+    )
 }
 
 export default HeroSection
