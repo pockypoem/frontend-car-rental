daa3ab28daf67518f070e20d832ca4765516b15e
pockypoem <jeremyas.cornelis@gmail.com>
2023-12-31 21:09:34 +0700

    chore: update nav text

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,18 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 
 type CustomNavbar = {
-    username: string,
+    // username: string,
     avatar : string
 }
 
 
-const CustomNavbar = ({ username, avatar } : CustomNavbar) => {
+const CustomNavbar = ({ avatar } : CustomNavbar) => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
                 <Navbar.Brand href="#home" style={{ fontSize: '30px' }}>Your Dashboard</Navbar.Brand>
                 <Nav className="ml-auto">
-                    <Nav.Link href="#home">Hello, {username}</Nav.Link>
+                    <Nav.Link href="#home">Hello </Nav.Link>
                     <Nav.Link href="#link">
                         <img 
                             src={avatar} 
